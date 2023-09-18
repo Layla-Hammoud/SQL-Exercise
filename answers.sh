78312b957c2cfe31e51b34d9f45c552d30bebe4f
Layla Hammoud <laylahammoud63@gmail.com>
2023-09-18 16:25:50 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -1,22 +1,22 @@
-SELECT name FROM students;
+1-SELECT name FROM students;
 
-SELECT * From students
+2-SELECT * From students
 Where Age > 30 ;
 
-SELECT name From students
+3-SELECT name From students
 WHERE age = 30 And gender = "F";
 
-SELECT points FROM students
+4-SELECT points FROM students
 WHERE name = "Alex";
 
-INSERT INTO students (name, age, Gender, points)
+5-INSERT INTO students (name, age, Gender, points)
 VALUES ('Layla', '22', 'F', '1000');
 
-UPDATE students
+6-UPDATE students
 SET Points = 500
 WHERE name = 'Basma';
 
-UPDATE students
+7-UPDATE students
 SET Points = 150
 WHERE name = 'Alex';
 
@@ -33,19 +33,51 @@ CREATE TABLE Graduates (
     Graduation DATE
 );
 
-INSERT INTO Graduates (Name, Age, Gender, Points)
+10-INSERT INTO Graduates (Name, Age, Gender, Points)
 SELECT Name, Age, Gender, Points
 FROM students
 WHERE name = 'Layal';
 
-UPDATE Graduates
+11-UPDATE Graduates
 SET Graduation = '2018-09-08'
 WHERE name = 'Layal';
 
-DELETE FROM students WHERE name = 'Layal';
+12-DELETE FROM students WHERE name = 'Layal';
 
 
 
-SELECT employees.name, employees.Company, companies.Date
+
+14-SELECT employees.name, employees.Company, companies.Date
 FROM employees
 INNER JOIN companies ON employees.Company = companies.name;
+
+15-SELECT employees.name
+FROM employees
+INNER JOIN companies ON employees.Company = companies.name
+WHERE companies.Date < 2000;
+
+16-SELECT companies.name
+FROM companies
+INNER JOIN employees ON companies.name = employees.Company
+WHERE employees.Role = 'Graphic Designer';
+
+
+
+18-SELECT name
+FROM students
+WHERE Points = (SELECT MAX(Points) FROM students);
+
+19-SELECT AVG(points)
+FROM students;
+
+20-SELECT COUNT(*) 
+FROM students
+WHERE points = 500;
+
+21-SELECT name
+FROM students
+WHERE name LIKE '%s%';
+
+22-SELECT *
+FROM students
+ORDER BY points DESC;
